e61dd29e0faf533c962177bb1c99d2d31c5b90e0
Jerónimo Restrepo <136396512+Jeroxin@users.noreply.github.com>
2024-11-02 07:09:14 -0500

    Primera parte modulo 1

---
+++ Javascript/Comentarios.js
@@ -0,0 +1,12 @@
+//Este es un comentario de una sola linea.
+/*
+Este 
+es
+un 
+comentario
+de
+varias
+lineas*/
+
+console.log("Mi Primer Hola Mundo!")
+console.log("%cHola mundo", "color:red; font-size:40px")
